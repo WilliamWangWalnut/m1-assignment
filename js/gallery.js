c264e5a7792b66548bca1018834049bb4291e787
[WilliamWangWalnutTheAdvantureCat] <[william.wang1986@icloud.com]>
2023-01-30 16:15:47 -0600

    box showing up, and able to close, but pop up box only showing first pic

--- js/gallery.js
+++ js/gallery.js
@@ -3,22 +3,24 @@ var photos = []; //Declare an empty array to store image element
 var fileNames = []; //To store image file names
 var imageList = []; //To store html list that contain an image
 var image; //To store the assemble image list codes
+var buttons = []; //To store the list of description button
 var openDiv = "<div class='column'>";
 var closeDiv = "</div>"
 var openList = "<li class='photo'>"; //Declare a variable to contain open list tag
 var closeList = "</li>"; //Declare a variable to contain close list tag
 var opencaption = "<div class='caption'>";
 var closecaption = "</div>";   
-var openDesc = "<div class='description'>";  
+var openDesc = "<div id='description'>";  
 var closeDesc = "</div>";
+
 
 //Create a loop to create 12 images starting with index of 0
 for (var i=0; i<12; i++) {
     fileNames.push ("cat" + (i+1)); //Create image file name and store in the array
     photos.push("<img src='images/" + fileNames[i] + ".jpg'>"); //Assembl file name into image element
-    
+
     image = openDiv + openList + photos[i] + closeList + opencaption + 
-    fileNames[i] + closecaption + openDesc + "<p>This is " + fileNames[i] + "</p>" + 
+    fileNames[i] + closecaption + openDesc + "<p id = 'myBtn' onclick='displayInfoBox()'>This is " + fileNames[i] + "</p>" + 
     closeDesc + closeDiv; //Assemble image element from array with list elements and store in a variable
     
     imageList.push(image); //Store(push) the assembled list codes into an array
--- js/infoBox.js
+++ js/infoBox.js
@@ -1,16 +1,17 @@
 //3. Script for Info Box (id="infoBox")
-var openSpan = "<span id='boxinbox'>";
-var closeSpan = "</span>"
+var openSpan = "<div id='boxinbox'>";
+var closeSpan = "</div>"
 var openHead = "<h2 id='infoHead'>";
 var closeHead = "</h2>";
 var openPara = "<p id='infoPara'>";
-var closePara = "</p>";
-var openLink = "<li id='infoLink'> Click This to Close";
-var closeLink = "</li>";
+var closePara = "</p><br></br><br></br>";
+var openLink = "<a id='infoLink'> Click This to Close";
+var closeLink = "</a>";
 var headNames = []; //To store caption/head names
 var paraContent = []; //To store paragraph <p> contents
 var box;
 var boxList = []; //To store final content inside box
+var x = document.getElementById("infoLink");
 
 //3.1. Create a loop to create 12 Caption & Paragraph, then store in "boxList"
 for (var i=0; i<12; i++) {
@@ -25,9 +26,15 @@ document.getElementById("infoBox").innerHTML = boxList.join(" ")
 
 
 
-//3.2. Make <div class='description' a button to display infoBox
-document.getElementsByClassName("description").onclick = displayInfoBox;
-
+//3.2. Make <p id='myBtn'> a button to display infoBox
 function displayInfoBox() {
-    document.getElementById()
+    document.getElementById("testing").innerHTML = Date();
+    document.getElementById("boxinbox").style.display = "inline";
+}
+
+//3.3. Make <a id='infoLink'> a button to close infoBox
+var closeButton = document.getElementById("infoLink");
+closeButton.onclick = closeInfoBox;
+function closeInfoBox() {
+    document.getElementById("boxinbox").style.display = "none";
 }
