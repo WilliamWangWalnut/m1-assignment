e4dc85c2612f73d849cbab0cb9b12d488b4c24b9
[WilliamWangWalnutTheAdvantureCat] <[william.wang1986@icloud.com]>
2023-01-30 19:45:24 -0600

    This is as far as I can go: Click description will display all boxes, all Click to Close works. Need to work out how to match description vs. box

--- js/gallery.js
+++ js/gallery.js
@@ -10,17 +10,17 @@ var openList = "<li class='photo'>"; //Declare a variable to contain open list t
 var closeList = "</li>"; //Declare a variable to contain close list tag
 var opencaption = "<div class='caption'>";
 var closecaption = "</div>";   
-var openDesc = "<div id='description'>";  
+var openDesc = "<div id='description' onclick='displayInfoBox()'>";  
 var closeDesc = "</div>";
 
 
 //Create a loop to create 12 images starting with index of 0
 for (var i=0; i<12; i++) {
     fileNames.push ("cat" + (i+1)); //Create image file name and store in the array
-    photos.push("<img src='images/" + fileNames[i] + ".jpg'>"); //Assembl file name into image element
+    photos.push("<img src='images/" + fileNames[i] + ".jpg'>"); //Assemble file name into image element
 
     image = openDiv + openList + photos[i] + closeList + opencaption + 
-    fileNames[i] + closecaption + openDesc + "<p id = 'myBtn' onclick='displayInfoBox()'>This is " + fileNames[i] + "</p>" + 
+    fileNames[i] + closecaption + openDesc + "<p id = 'myBtn'>This is " + fileNames[i] + "</p>" + 
     closeDesc + closeDiv; //Assemble image element from array with list elements and store in a variable
     
     imageList.push(image); //Store(push) the assembled list codes into an array
--- js/infoBox.js
+++ js/infoBox.js
@@ -5,7 +5,7 @@ var openHead = "<h2 id='infoHead'>";
 var closeHead = "</h2>";
 var openPara = "<p id='infoPara'>";
 var closePara = "</p><br></br><br></br>";
-var openLink = "<a id='infoLink'> Click This to Close";
+var openLink = "<a id='infoLink' onclick = 'closeInfoBox()'> Click This to Close";
 var closeLink = "</a>";
 var headNames = []; //To store caption/head names
 var paraContent = []; //To store paragraph <p> contents
@@ -17,7 +17,8 @@ var x = document.getElementById("infoLink");
 for (var i=0; i<12; i++) {
     headNames.push ("cat" + (i+1));
     paraContent.push ("This is " + headNames[i] + ". He is a male cat at age " + (i+1) + ".")
-    box = openSpan + openHead + headNames[i] + closeHead + openPara + paraContent[i] + closePara + openLink + closeLink + closeSpan; 
+    box = openSpan + openHead + headNames[i] + closeHead + openPara + paraContent[i] 
+    + closePara + openLink + closeLink + closeSpan; 
     boxList.push(box);
 }
 //Display Caption & Paragraph in "infoBox"
@@ -28,13 +29,10 @@ document.getElementById("infoBox").innerHTML = boxList.join(" ")
 
 //3.2. Make <p id='myBtn'> a button to display infoBox
 function displayInfoBox() {
-    document.getElementById("testing").innerHTML = Date();
-    document.getElementById("boxinbox").style.display = "inline";
+    document.getElementById("infoBox").style.display = "inline";
 }
 
 //3.3. Make <a id='infoLink'> a button to close infoBox
-var closeButton = document.getElementById("infoLink");
-closeButton.onclick = closeInfoBox;
 function closeInfoBox() {
-    document.getElementById("boxinbox").style.display = "none";
+    document.getElementById("infoBox").style.display = "none";
 }
